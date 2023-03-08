c1f8c49b585c53a026af405da00d7aa6e1636d4a
Kaiz <kaiz@kaiz.jp>
2023-03-08 23:40:43 +0900

    first commit

---
+++ src/main/java/jp/kaiz/minervic/Minervic.kt
@@ -0,0 +1,12 @@
+package jp.kaiz.minervic
+
+import cpw.mods.fml.common.Mod
+
+@Mod(modid = MinervIC.MODID, name = MinervIC.NAME, version = MinervIC.VERSION)
+class MinervIC {
+    companion object {
+        const val MODID = "minervic"
+        const val NAME = "MinervIC"
+        const val VERSION = "1.0"
+    }
+}
